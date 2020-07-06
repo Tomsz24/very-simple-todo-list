9d199026ced19584b854fadf315fbccb9b98b5b5
Tomasz Wojciechowski <tomaszwojciechowski244@gmail.com>
2020-07-06 16:35:54 +0200

    modyfied app adding two buttons

--- main.js
+++ main.js
@@ -5,12 +5,43 @@ const tasks = [
   }
 ]
 
+let hidingMode = false;
+
+const buttonsRender = () => {
+  let htmlContent = "";
+  if (tasks.length > 0) {
+    htmlContent +=
+      `Lista zadań 
+      <button class="section__button--showHide">${hidingMode ? 'Pokaż wszystkie' : 'Ukryj ukończone'}</button>
+    <button class="section__button--allDone${tasks.every(task => task.done === true) ? ' --color' : ''}">Ukończ wszystkie</button>`
+
+
+  } else {
+    htmlContent +=
+      `Lista zadań `
+  }
+
+  document.querySelector('.section__subtitle').innerHTML = htmlContent;
+
+  const allDone = document.querySelector('.section__button--allDone');
+
+
+  allDone.addEventListener('click', allTasksDone);
+
+  const showHide = document.querySelector('.section__button--showHide');
+
+  showHide.addEventListener('click', toggleShowHide);
+
+}
+
+
+
 const render = () => {
   let htmlString = "";
 
   tasks.map(task => (
     htmlString +=
-    `<li class="task">
+    `<li class="task${task.done && hidingMode ? '--hide' : ''}">
       <button class="section__addTask">
         ${task.done ? '<i class="fas fa-check"></i>' : ''}
       </button>
@@ -25,6 +56,9 @@ const render = () => {
 
   document.querySelector('.taskList').innerHTML = htmlString;
   taskAllEvents();
+
+  buttonsRender();
+
 }
 
 const removeTask = index => {
@@ -44,6 +78,16 @@ const addNewTask = newTaskContent => {
   })
 }
 
+const allTasksDone = () => {
+  tasks.map(task => task.done = task.done = true)
+  render();
+}
+
+const toggleShowHide = () => {
+  hidingMode = !hidingMode;
+  render();
+}
+
 const taskAllEvents = () => {
   document.querySelectorAll('.section__addTask').forEach((button, index) => {
     button.addEventListener('click', () => {
@@ -56,11 +100,14 @@ const taskAllEvents = () => {
       removeTask(index);
     });
   });
+
+
 }
 
 
 const init = () => {
   render();
+
 
   const form = document.querySelector('.header__form');
   form.addEventListener('submit', event => {
